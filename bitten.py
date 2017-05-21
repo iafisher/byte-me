9f6d08d97e9778e74b1b7f3930dbcd88269812ce
Ian Fisher <ianfisher45@gmail.com>
2017-05-21 10:34:40 -0400

    Put each bytecode instruction in its own HTML element
    This will make it easier to implement the hover-over text, because each
    line of the bytecode has its own element (with the op name as its
    class) that can be targeted by the JavaScript code.

--- bitten.py
+++ bitten.py
@@ -1,4 +1,5 @@
 import dis
+from collections import namedtuple
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
@@ -7,13 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', name='index')
 
+BytecodeElement = namedtuple('BytecodeElement', ['string', 'code'])
+
 @app.route('/', methods=['POST'])
 def index_as_post():
     try:
         source_code = request.form['source_code']
         bytecode = dis.Bytecode(source_code)
+        bytecode_list = []
+        for inst, line in zip(bytecode, bytecode.dis().splitlines()):
+            bytecode_list.append(BytecodeElement(line, inst.opname))
         return render_template('index.html', name='index', source_code=source_code, 
-                                                           bytecode=bytecode.dis())
+                                                           bytecode_list=bytecode_list)
     except SyntaxError:
         return render_template('index.html', name='index', source_code=source_code,
                                                            error_msg='Syntax error')
