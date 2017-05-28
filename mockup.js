0c564342a6728fe44620f3eed94928cf291ee6a2
Ian Fisher <ianfisher45@gmail.com>
2017-05-28 19:44:20 -0400

    Improve styling of the bytecode instructions
    The bytecode instructions now line up with their corresponding lines of
    source code.

--- mockup.js
+++ mockup.js
@@ -2,7 +2,9 @@ var my_textarea = document.getElementById("source_code_textarea");
 var editor = CodeMirror.fromTextArea(my_textarea, {lineNumbers: true, mode:"python", smartIndent: true});
 
 $("#compile").click(function() {
-    $("#bytecodeRow").show();
+    $(".bytecode-row").show("slow");
+    /*
     var my_textarea2 = document.getElementById("submittedTextArea");
     var editor2 = CodeMirror.fromTextArea(my_textarea2, {mode:"python", readOnly:true});
+    */
 });
