8d3047560c9c6f79f87530d21ec888689823ccda
Ian Fisher <ianfisher45@gmail.com>
2017-05-29 13:44:56 -0400

    Link the frontend and backend with AJAX
    The basic functionality of the site is now complete; you can enter
    source code on the website and view the bytecode.

--- bitten.py
+++ bitten.py
@@ -1,5 +1,5 @@
 import dis
-from collections import namedtuple
+import json
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
@@ -8,18 +8,37 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', name='index')
 
-BytecodeElement = namedtuple('BytecodeElement', ['string', 'code'])
-
-@app.route('/', methods=['POST'])
-def index_as_post():
+@app.route('/bytecode', methods=['POST'])
+def bytecode_post():
+    ret = []
     try:
-        source_code = request.form['source_code']
+        source_code = request.form['sourceCode']
         bytecode = dis.Bytecode(source_code)
-        bytecode_list = []
-        for inst, line in zip(bytecode, filter(len, bytecode.dis().splitlines())):
-            bytecode_list.append(BytecodeElement(line, inst.opname))
-        return render_template('index.html', name='index', source_code=source_code, 
-                                                           bytecode_list=bytecode_list)
     except SyntaxError:
-        return render_template('index.html', name='index', source_code=source_code,
-                                                           error_msg='Syntax error')
+        pass
+    else:
+        for line, inst_group in zip(source_code.splitlines(), group_bytecode(bytecode)):
+            ret.append({'source':line, 'bytecode':list(map(inst_to_str, inst_group))})
+    return json.dumps(ret)
+
+def inst_to_str(inst):
+    """Convert a bytecode instruction to a string."""
+    if inst.arg is not None:
+        padding = ' ' * (25 - len(inst.opname))
+        return '{0.opname}{1}{0.arg} ({0.argrepr})'.format(inst, padding)
+    else:
+        return inst.opname
+
+def group_bytecode(bytecode):
+    """Yield tuples of bytecode instructions, with each tuple matching a single line of source
+       code.
+    """
+    collect = []
+    for inst in bytecode:
+        if inst.starts_line:
+            if collect:
+                yield tuple(collect)
+            collect.clear()
+        collect.append(inst)
+    if collect:
+        yield tuple(collect)
---
+++ static/index.js
@@ -0,0 +1,43 @@
+var my_textarea = document.getElementById("sourceCodeTextarea");
+var editor = CodeMirror.fromTextArea(my_textarea, {lineNumbers: true, mode:"python", smartIndent: true});
+
+$("#compile").click(function() {
+    $.ajax({
+        url: "/bytecode",
+        data: {sourceCode: editor.getValue()},
+        dataType: "json", 
+        method: "POST",
+        success: addBytecode,
+    });
+});
+
+function addBytecode(data) {
+    if (data.length > 0) {
+        $(".bytecode-row").detach();
+        for (var i = 0; i < data.length; i++) {
+            $("#mainContainer").append(createBytecodeRow(data[i]));
+        }
+    }
+}
+
+function createBytecodeRow(codeObj) {
+    // I hope there's a better way to do this
+    var row = document.createElement("div");
+    row.setAttribute("class", "row bytecode-row");
+    var emptyColumn = document.createElement("div");
+    emptyColumn.setAttribute("class", "col-md-3");
+    var sourceColumn = document.createElement("div");
+    sourceColumn.setAttribute("class", "col-md-3 code-pair");
+    sourceColumn.appendChild(document.createTextNode(codeObj.source));
+    var byteColumn = document.createElement("div");
+    byteColumn.setAttribute("class", "col-md-3 code-pair");
+    for (var i = 0; i < codeObj.bytecode.length; i++) {
+        var preElement = document.createElement("pre");
+        preElement.appendChild(document.createTextNode(codeObj.bytecode[i]));
+        byteColumn.appendChild(preElement);
+    }
+    row.appendChild(emptyColumn);
+    row.appendChild(sourceColumn);
+    row.appendChild(byteColumn);
+    return row;
+}
