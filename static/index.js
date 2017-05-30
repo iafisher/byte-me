ffb9ab63dae2b445e4d27e865c4051d96b36d88e
Noah Kim <nk8na@virginia.edu>
2017-05-30 09:13:57 -0400

    Make table for the code
    There might be some corner cases I missed but it is generally functional.

--- static/index.js
+++ static/index.js
@@ -19,8 +19,23 @@ function addBytecode(data) {
             $("#syntaxError").text("");
             // remove all previous bytecode elements
             $(".bytecode-row").detach();
+	    //create row, col, table
+	    var row = document.createElement("div");
+	    row.setAttribute("class", "row bytecode-row justify-content-center");
+	    var col = document.createElement("div");
+	    col.setAttribute("class", "col-md-6");
+	    var table = document.createElement("table");
+	    table.id="bytecode-table";
+	    table.setAttribute("class", "table table-hover");
+	    col.appendChild(table);
+	    row.appendChild(col);
+	    $("#mainContainer").append(row);
+	    document.getElementById("bytecode-table").innerHTML='<thead><tr><th>Bytecode</th><th>opname</th><th>arg</th><th>argrepr</th></tr></thead>';
+	    var body = document.createElement("tbody");
+	    body.id = "tableBody";
+	    table.append(body);
             for (var i = 0; i < data.length; i++) {
-                $("#mainContainer").append(createBytecodeRow(data[i]));
+                createBytecodeRow(data[i]);
             }
         }
     } else {
@@ -30,22 +45,65 @@ function addBytecode(data) {
 
 function createBytecodeRow(codeObj) {
     // I hope there's a better way to do this
-    var row = document.createElement("div");
-    row.setAttribute("class", "row bytecode-row");
-    var emptyColumn = document.createElement("div");
-    emptyColumn.setAttribute("class", "col-md-3");
-    var sourceColumn = document.createElement("div");
-    sourceColumn.setAttribute("class", "col-md-3 code-pair");
-    sourceColumn.appendChild(document.createTextNode(codeObj.source));
-    var byteColumn = document.createElement("div");
-    byteColumn.setAttribute("class", "col-md-3 code-pair");
-    for (var i = 0; i < codeObj.bytecode.length; i++) {
-        var preElement = document.createElement("pre");
-        preElement.appendChild(document.createTextNode(codeObj.bytecode[i]));
-        byteColumn.appendChild(preElement);
+    // table.appendChild(content);
+    var tablerow =  document.createElement("tr");
+    var source = document.createElement("td");
+    var opname = document.createElement("td");
+    var arg = document.createElement("td");
+    var argrepr = document.createElement("td");
+    source.setAttribute("scope", "row");
+    source.innerHTML = codeObj.source;
+    opname.innerHTML = codeObj.bytecode[0].opname;
+    arg.innerHTML = codeObj.bytecode[0].arg;
+    argrepr.innerHTML = codeObj.bytecode[0].arg;
+    tablerow.append(source);
+    tablerow.append(opname);
+    tablerow.append(arg);
+    tablerow.append(argrepr);
+    $("#tableBody").append(tablerow);
+    for (var i = 1; i <codeObj.bytecode.length; i++) {
+	var extraTablerow = document.createElement("tr");
+	var extraOpname = document.createElement("td");
+	var extraArg = document.createElement("td");
+	var extraArgrepr = document.createElement("td");
+	if(typeof codeObj.bytecode[i]=='object'){
+	    extraOpname.innerHTML = codeObj.bytecode[i].opname;
+	    extraArg.innerHTML = codeObj.bytecode[i].arg;
+	    extraArgrepr.innerHTML = codeObj.bytecode[i].arg;
+	    extraTablerow.append(document.createElement("td"));
+	    extraTablerow.append(extraOpname);
+	    extraTablerow.append(extraArg);
+	    extraTablerow.append(extraArgrepr);
+	    $("#tableBody").append(extraTablerow);
+	}
+	else{
+	    extraOpname.innerHTML = codeObj.bytecode[i];
+	    extraArg.innerHTML = codeObj.bytecode[i].arg;
+	    extraArgrepr.innerHTML = codeObj.bytecode[i].arg;
+	    extraTablerow.append(document.createElement("td"));
+	    extraTablerow.append(extraOpname);
+	    extraTablerow.append(document.createElement("td"));
+	    extraTablerow.append(document.createElement("td"));
+	}
+	$("#tableBody").append(extraTablerow);
     }
-    row.appendChild(emptyColumn);
-    row.appendChild(sourceColumn);
-    row.appendChild(byteColumn);
-    return row;
+    return tablerow;
+
+
+
+
+
+    
+   // sourceColumn.appendChild(document.createTextNode(codeObj.source));
+   // var byteColumn = document.createElement("div");
+   // byteColumn.setAttribute("class", "col-md-3 code-pair");
+   // for (var i = 0; i < codeObj.bytecode.length; i++) {
+   //     var preElement = document.createElement("pre");
+   //     preElement.appendChild(document.createTextNode(codeObj.bytecode;
+   //     byteColumn.appendChild(preElement);
+   // }
+   // row.appendChild(table);
+   // row.appendChild(sourceColumn);
+   // row.appendChild(byteColumn);
+   // return row;
 }
