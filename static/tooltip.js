832d41a0caf459f80da356c716483bf63f5e86ac
Ian Fisher <ianfisher45@gmail.com>
2017-05-31 10:37:08 -0400

    Clean up repository
    This commit removes the mockup files, the old code from index.js, some
    defunct CSS, and renames the tooltip.js functions with camel-case per
    normal JS style.

--- static/tooltip.js
+++ static/tooltip.js
@@ -1,20 +1,21 @@
-function register_tooltip(classname, text) {
-    $('.' + classname).attr('title', text);
-}
 
 // General Instructions
-function register_tooltips(){
-    register_tooltip('NOP', '');
-    register_tooltip('POP_TOP', '');
-    register_tooltip('ROT_TWO', '');
-    register_tooltip('ROT_THREE', '');
-    register_tooltip('DUP_TOP', '');
-    register_tooltip('DUP_TOP_TWO', '');
+function registerTooltips(){
+    registerTooltip('NOP', '');
+    registerTooltip('POP_TOP', '');
+    registerTooltip('ROT_TWO', '');
+    registerTooltip('ROT_THREE', '');
+    registerTooltip('DUP_TOP', '');
+    registerTooltip('DUP_TOP_TWO', '');
     // Unary operations
-    register_tooltip('UNARY_POSITIVE', '');
-    register_tooltip('UNARY_NEGATIVE', '');
-    register_tooltip('UNARY_NOT', '');
-    register_tooltip('UNARY_INVERT', '');
-    register_tooltip('GET_ITER', '');
-    register_tooltip('LOAD_NAME', 'Push the value that name refers to onto the stack');
+    registerTooltip('UNARY_POSITIVE', '');
+    registerTooltip('UNARY_NEGATIVE', '');
+    registerTooltip('UNARY_NOT', '');
+    registerTooltip('UNARY_INVERT', '');
+    registerTooltip('GET_ITER', '');
+    registerTooltip('LOAD_NAME', 'Push the value that name refers to onto the stack');
+}
+
+function registerTooltip(classname, text) {
+    $('.' + classname).attr('title', text);
 }
