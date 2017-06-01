cd804838f6cc1bd06ec17c29e4a3866a8351e4e9
Ian Fisher <ianfisher45@gmail.com>
2017-06-01 14:03:08 -0400

    Fix bug #13 and reorganize modules
    This commit separates the bytecode-extraction code into a separate
    module from the Flask and JSON-serialization code.

--- bitten.py
+++ bitten.py
@@ -1,86 +1,38 @@
-"""The module for handling the Flask stuff.
-
-The website boils down a single HTML page and a POST handler that takes in source code and spits
-out a code package that pairs each line of source code with its matching CPython bytecode
-instructions. For details, see the bytecode_post function.
-"""
 import dis
-import json
-import html
-import itertools
+from collections import namedtuple
 
-from flask import Flask, render_template, request
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    """Render the homepage."""
-    return render_template('index.html', name='index')
-
-@app.route('/bytecode', methods=['POST'])
-def bytecode_post():
-    """Handle POST requests of source code by returning the compiled bytecode as JSON. A sample
-       return value is
-
-       [
-        {'name':'<module>', 
-         'package':[
-                    {'source':'x = 5',
-                     'bytecode' [
-                                 {'arg': '0', 'argrepr': '5', 'opname': 'LOAD_CONST'}, 
-                                 ...
-                                ]
-                     },
-                     ...
-                    ]
-        },
-        ...
-       ]
-
+def package_module(source):
+    """Return a list of CodePackage objects for each function in the source code, as well as a
+       package for the module as a whole.
     """
-    source = request.form['sourceCode']
-    try:
-        module_bytecode = dis.Bytecode(source)
-    except SyntaxError as e:
-        return json.dumps('Syntax error at line {}'.format(e.lineno))
-    functions = extract_functions(module_bytecode)
-    ret = [package_code(f.co_name, extract_source_code(source.splitlines(), f), dis.Bytecode(f)) 
-               for f in functions]
-    # prepend the <module> code so that it shows up first on the website
-    ret = [package_code('<module>', source.splitlines(), module_bytecode)] + ret
-    return html.escape(json.dumps(ret), quote=False)
+    module_bytecode = dis.Bytecode(source)
+    source_lines = source.splitlines()
+    module_package = CodePackage('<module>', source_lines, module_bytecode)
+    functions = [CodePackage.fromfunction(source_lines, f)
+                    for f in extract_functions(module_bytecode.codeobj)]
+    return [module_package] + functions
 
-def package_code(name, source_code, bytecode):
-    ret = []
-    for lineno, byte_group in group_bytecode(bytecode):
-        try:
-            ret.append({'source': source_code[lineno],
-                        'bytecode': list(map(instruction_to_json, byte_group))})
-        except IndexError:
-            print(source_code, lineno)
-    return {'name':name, 'package':ret}
 
-def extract_functions(bytecode):
-    """Return a list of function code objects that are defined in the bytecode."""
-    code_object = bytecode.codeobj
-    code_type = type(code_object)
-    functions = [x for x in code_object.co_consts if isinstance(x, code_type)]
-    return functions
+class CodePackage:
+    """Source code paired with bytecode instructions."""
+    
+    def __init__(self, name, source_lines, bytecode):
+        self.name = name
+        self.code_pairs = [CodePair(source_lines[n], g) for n, g in group_bytecode(bytecode)]
 
-def extract_source_code(source_lines, f_code):
-    """Given the source code for an entire module and the code object of a single function, return
-       the source code of that function as a list of strings.
-    """
-    bytecode = dis.Bytecode(f_code)
-    last_line = max(i.starts_line for i in bytecode if i.starts_line is not None)
-    return source_lines[bytecode.first_line : last_line + 1]
+    @classmethod
+    def fromfunction(cls, source_lines, f):
+        """Construct a code package from the source code of an entire module and a code object of
+           a function defined within that module.
+        """
+        bytecode = dis.Bytecode(f)
+        # change the first line from the 'def ...' line to the first line of the body
+        bytecode.first_line += 1
+        return cls(f.co_name, source_lines, bytecode)
 
-def instruction_to_json(inst):
-    """Convert a bytecode instruction to a JSON-serializable object."""
-    if inst.arg is not None:
-        return {'opname': inst.opname, 'arg': str(inst.arg), 'argrepr': inst.argrepr}
-    else:
-        return {'opname': inst.opname, 'arg': "", 'argrepr': ""}
+
+CodePair = namedtuple('CodePair', ['source', 'bytecode'])
+
 
 def group_bytecode(bytecode):
     """Yield (lineno, (<bytecode instructions>)) tuples for the bytecode object. The line numbers
@@ -96,3 +48,15 @@ def group_bytecode(bytecode):
         collect.append(instruction)
     if collect:
         yield (last_line - 1, tuple(collect))
+
+def extract_functions(codeobj):
+    """Return a list of all functions defined in the code object, including nested function
+       definitions.
+    """
+    code_type = type(codeobj)
+    ret = []
+    for x in codeobj.co_consts:
+        if isinstance(x, code_type):
+            ret.append(x)
+            ret.extend(extract_functions(x))
+    return ret
---
+++ webinter.py
@@ -0,0 +1,126 @@
+"""The module for handling the Flask stuff.
+
+The website boils down a single HTML page and a POST handler that takes in source code and spits
+out a code package that pairs each line of source code with its matching CPython bytecode
+instructions. For details, see the bytecode_post function.
+"""
+import dis
+import json
+import html
+import itertools
+
+from bitten import CodePackage, CodePair, package_module
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Render the homepage."""
+    return render_template('index.html', name='index')
+
+@app.route('/bytecode', methods=['POST'])
+def bytecode_post():
+    """Handle POST requests of source code by returning the compiled bytecode as JSON. A sample
+       return value is
+
+       [
+        {'name':'<module>', 
+         'package':[
+                    {'source':'x = 5',
+                     'bytecode' [
+                                 {'arg': '0', 'argrepr': '5', 'opname': 'LOAD_CONST'}, 
+                                 ...
+                                ]
+                     },
+                     ...
+                    ]
+        },
+        ...
+       ]
+
+    """
+    packlist = package_module(request.form['sourceCode'])
+    ret = json.dumps(json_comply(packlist))
+    return html.escape(ret, quote=False)
+
+
+def json_comply(packlist):
+    return [json_comply_code_package(package) for package in packlist]
+
+def json_comply_code_package(package):
+    return {'name':package.name, 'package':json_comply_code_pairs(package.code_pairs)}
+
+def json_comply_code_pairs(code_pairs):
+    return [{'source':p.source, 'bytecode':json_comply_bytecode(p.bytecode)} for p in code_pairs]
+
+def json_comply_bytecode(bytecode_list):
+    ret = []
+    for inst in bytecode_list:
+        if inst.arg is not None:
+            ret.append({'opname':inst.opname, 'arg':str(inst.arg), 'argrepr':inst.argrepr})
+        else:
+            ret.append({'opname':inst.opname, 'arg':'', 'argrepr':''})
+    return ret
+
+
+'''
+    source = request.form['sourceCode']
+    try:
+        module_bytecode = dis.Bytecode(source)
+    except SyntaxError as e:
+        return json.dumps('Syntax error at line {}'.format(e.lineno))
+    functions = extract_functions(module_bytecode)
+    ret = [package_code(f.co_name, extract_source_code(source.splitlines(), f), dis.Bytecode(f)) 
+               for f in functions]
+    # prepend the <module> code so that it shows up first on the website
+    ret = [package_code('<module>', source.splitlines(), module_bytecode)] + ret
+    return html.escape(json.dumps(ret), quote=False)
+
+def package_code(name, source_code, bytecode):
+    ret = []
+    for lineno, byte_group in group_bytecode(bytecode):
+        try:
+            ret.append({'source': source_code[lineno],
+                        'bytecode': list(map(instruction_to_json, byte_group))})
+        except IndexError:
+            print(source_code, lineno)
+    return {'name':name, 'package':ret}
+
+def extract_functions(bytecode):
+    """Return a list of function code objects that are defined in the bytecode."""
+    code_object = bytecode.codeobj
+    code_type = type(code_object)
+    functions = [x for x in code_object.co_consts if isinstance(x, code_type)]
+    return functions
+
+def extract_source_code(source_lines, f_code):
+    """Given the source code for an entire module and the code object of a single function, return
+       the source code of that function as a list of strings.
+    """
+    bytecode = dis.Bytecode(f_code)
+    last_line = max(i.starts_line for i in bytecode if i.starts_line is not None)
+    return source_lines[bytecode.first_line : last_line + 1]
+
+def instruction_to_json(inst):
+    """Convert a bytecode instruction to a JSON-serializable object."""
+    if inst.arg is not None:
+        return {'opname': inst.opname, 'arg': str(inst.arg), 'argrepr': inst.argrepr}
+    else:
+        return {'opname': inst.opname, 'arg': "", 'argrepr': ""}
+
+def group_bytecode(bytecode):
+    """Yield (lineno, (<bytecode instructions>)) tuples for the bytecode object. The line numbers
+       are zero-indexed.
+    """
+    collect = []
+    last_line = bytecode.first_line
+    for instruction in bytecode:
+        if instruction.starts_line and collect:
+            yield (last_line - 1, tuple(collect))
+            collect.clear()
+            last_line = instruction.starts_line
+        collect.append(instruction)
+    if collect:
+        yield (last_line - 1, tuple(collect))
+'''
