094cae61c3a055e07a2cf8e58760c1d98071bc1b
Ian Fisher <ianfisher45@gmail.com>
2017-06-09 16:59:27 -0400

    Simplify some of the JS

--- static/index.js
+++ static/index.js
@@ -12,7 +12,7 @@ $("#compile").click(function() {
         data: {sourceCode: editor.getValue()},
         dataType: "json",
         method: "POST",
-        success: function(data){
+        success: function(data) {
 	    addBytecode(data);
 	}
     });
@@ -22,26 +22,24 @@ function addBytecode(data) {
     if (typeof data !== "string") {
         // clear the error box
         $("#syntaxError").text("");
-        // remove all tabs
+        // remove all tabs and tab panels
         $("#tabs").empty();
-        // remove the previous bytecode table, if it existed
         $("#tabContent").empty();
         // make the new tab headers
-        var regTabNum = Math.min(data.length, 6)
+        var regTabNum = Math.min(data.length, 6);
         for (var i = 0; i < regTabNum; i++) {
             $("#tabs").append(makeTabHeader(data[i]['name'], i));
         }
-        var button = '<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>'
-        var firstDropdown = '<ul class="dropdown-menu" id = "dropdown"></ul>'
-        var dropdown = '<li class = "dropdown">' + button + firstDropdown + '</li>';
-        if(data.length>=6){
-          $("#tabs").append(dropdown)
+        if (data.length >= 6) {
+            $("#tabs").append(makeDropdownList());
+            for (var i = 6; i < data.length; i++) {
+                $("#dropdownList").append(makeTabHeader(data[i]['name'], i));
+            }
         }
-        for( var i = 6; i < data.length; i++) {
-            $("#dropdown").append(makeTabHeader(data[i]['name'], i));
+        // make the new tab panels
+        for (var i = 0; i < data.length; i++) {
+            $("#tabContent").append(makeTabPanel(data[i]['package'], i));
         }
-        // make the new tab panels (with bytecode panels)
-        $("#tabContent").append(makeTabPanels(data));
         // activate the first tab
         $("#tabs li:first").addClass("active");
         $("#tabContent div:first").addClass("active");
@@ -50,33 +48,25 @@ function addBytecode(data) {
     }
 }
 
-// Make the tab panels, each of which containing a bytecode table
-function makeTabPanels(data) {
-    var tabsArray = data.map(function(x, i) {
-        var table = makeTable(x['package']);
-        return '<div id="tab' + (i + 1) + '" class="tab-pane">' + table + '</div>';
-    });
-    return tabsArray.join('');
-}
-
 // Make a single tab header
 function makeTabHeader(name, i) {
     return '<li><a href="#tab' + (i + 1) + '" data-toggle="tab">' + name + '</a></li>';
 }
 
-// Make a bytecode table from the code package
-function makeTable(pack) {
-    var thead = '<thead><tr><th>Line</th><th>Source Code</th><th>Opname</th><th>Description</th></tr></thead>'
+// Make a tab panel from the code package
+function makeTabPanel(pack, i) {
+    var thead = '<thead><tr><th>Line</th><th>Source Code</th><th>Opname</th><th>Description</th></tr></thead>';
     var tbody = '<tbody>' + makeTableBody(pack) + '</tbody>';
-    return '<table class="table table-hover bytecode-table">' + thead + tbody + '</table>';
+    var table = '<table class="table table-hover bytecode-table">' + thead + tbody + '</table>';
+    return '<div id="tab' + (i + 1) + '" class="tab-pane">' + table + '</div>';
 }
 
 // Make the body of a table from the code package
 function makeTableBody(pack) {
-    return pack.map(rowsFromCodePack).join("");
+    return pack.map(makeRowGroup).join("");
 }
 
-function rowsFromCodePack(code) {
+function makeRowGroup(code) {
     var firstRow = makeTableRow(code.source, code.lineno, code.bytecode[0]);
     var otherRows = code.bytecode.slice(1).map(function (b) { return makeTableRow('', '', b); });
     return firstRow + otherRows.join("");
@@ -85,4 +75,11 @@ function rowsFromCodePack(code) {
 function makeTableRow(source, lineno, bytecode) {
     var desc = getDescription(bytecode);
     return '<tr><td>' + lineno + '<td class="source">' + source + '</td><td class="opname">' + bytecode.opname + '</td><td class="desc">' + desc + '</td></tr>';
+}
+
+// Make the dropdown list (a static HTML element)
+function makeDropdownList() {
+    var button = '<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>';
+    var firstDropdown = '<ul class="dropdown-menu" id="dropdownList"></ul>';
+    return '<li class="dropdown">' + button + firstDropdown + '</li>';
 }
